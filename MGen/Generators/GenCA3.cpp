1f86b88deda2473ec15c0990f2168ec4835a964c
Rualark <rualark@gmail.com>
2018-11-02 10:04:26 +0300

    Intermediate commit

--- MGen/Generators/GenCA3.cpp
+++ MGen/Generators/GenCA3.cpp
@@ -578,7 +578,7 @@ void CGenCA3::SaveLy(CString dir, CString fname) {
 		}
 	}
 	ly_fs << ly_ly_st;
-	ly_fs << "\\header {tagline = \"This file was created by MGen CP2 ";
+	ly_fs << "\\header {tagline = \"This file was created by MGen CA3 ";
 	ly_fs << APP_VERSION << " at " << CTime::GetCurrentTime().Format("%Y-%m-%d %H:%M:%S") << "\"}\n";
 	read_file_sv("configs\\ly\\footer.ly", sv);
 	write_file_sv(ly_fs, sv);
