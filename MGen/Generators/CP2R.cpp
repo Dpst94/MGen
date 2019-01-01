b5982460da77f21db3f93d626750b34f7c8d3d2b
Rualark <rualark@gmail.com>
2019-01-01 21:30:31 +0300

    CA3: Prohibit far chromaticism independently from the number of chords in between

--- MGen/Generators/CP2R.cpp
+++ MGen/Generators/CP2R.cpp
@@ -1056,8 +1056,7 @@ int CP2R::FailMinor() {
 			}
 			if (ls > 1) {
 				s_2 = fli[v][ls - 2];
-				if (pcc[v][s_2] == 10 && 
-					bhli[s] - bhli[s_2] == 1) FlagVL(v, 159, s_2, s);
+				if (pcc[v][s_2] == 10) FlagVL(v, 159, s_2, s);
 			}
 			if (ls < fli_size[v] - 1) {
 				s1 = fli[v][ls + 1];
@@ -1066,8 +1065,7 @@ int CP2R::FailMinor() {
 				if (pcc[v][s1] == 3) FlagVL(v, 156, s1, s);
 				if (ls < fli_size[v] - 2) {
 					s2 = fli[v][ls + 2];
-					if (pcc[v][s2] == 10 && 
-						bhli[s2] - bhli[s] == 1) FlagVL(v, 159, s2, s);
+					if (pcc[v][s2] == 10) FlagVL(v, 159, s2, s);
 				}
 			}
 		}
@@ -1076,8 +1074,7 @@ int CP2R::FailMinor() {
 			if (pcc[v][s_1] == 3) FlagVL(v, 155, s_1, s);
 			if (ls > 1) {
 				s_2 = fli[v][ls - 2];
-				if (pcc[v][s_2] == 8 && 
-					bhli[s] - bhli[s_2] == 1) FlagVL(v, 158, s_2, s);
+				if (pcc[v][s_2] == 8) FlagVL(v, 158, s_2, s);
 			}
 			if (ls < fli_size[v] - 1) {
 				s1 = fli[v][ls + 1];
@@ -1085,8 +1082,7 @@ int CP2R::FailMinor() {
 				if (pcc[v][s1] == 3) FlagVL(v, 155, s1, s);
 				if (ls < fli_size[v] - 2) {
 					s2 = fli[v][ls + 2];
-					if (pcc[v][s2] == 8 && 
-						bhli[s2] - bhli[s] == 1) FlagVL(v, 158, s2, s);
+					if (pcc[v][s2] == 8) FlagVL(v, 158, s2, s);
 				}
 			}
 		}
