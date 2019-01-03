3659821626ea025e25ae70ac7462e27bef92a4f7
Rualark <rualark@gmail.com>
2019-01-03 13:47:54 +0300

    CA3: Part name is out of page #2505

--- MGen/Generators/CP2Ly.cpp
+++ MGen/Generators/CP2Ly.cpp
@@ -609,7 +609,7 @@ void CP2Ly::SaveLyCP() {
 			clef = vocra_info[vocra[v]].clef;
 		}
 		ly_ly_st += "     \n";
-		st.Format("  \\set Staff.instrumentName = \\markup { \\tiny \\override #'(baseline-skip . 2.0) \\center-column{ \"%s\" \"[%s]\"%s } }\n", vname2[vi], st3, vocra_st);
+		st.Format("  \\set Staff.instrumentName = \\markup { \\teeny \\override #'(baseline-skip . 2.0) \\center-column{ \"%s\" \"[%s]\"%s } }\n", vname2[vi].Mid(0, 10), st3, vocra_st);
 		//ly_ly_st += "    \\override #'(line-width . 100)  \n";
 		ly_ly_st += st;
 		ly_ly_st += "  \\clef \"" + clef + "\"\n  \\key " + LyMajorKey[maj_bn] + "\n";
