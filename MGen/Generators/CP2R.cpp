7fcb4a9f0ba9de7ab2dbbaae6d962166e19cfd01
Rualark <rualark@gmail.com>
2019-03-24 09:16:23 +0300

    CA3: Increase penalty for 7th chord #2667

--- MGen/Generators/CP2R.cpp
+++ MGen/Generators/CP2R.cpp
@@ -4196,7 +4196,7 @@ void CP2R::EvalMshHarm(int hvar) {
 	}
 	// Flag 7th chord
 	if (found_de4) 
-		FlagA(0, 194, hstart, hstart, 0, 80);
+		FlagA(0, 194, hstart, hstart, 0, 500);
 	// Prohibit DTIII#5 augmented chord (if not 7th chord, because 7th chord is already an error)
 	if (cchnv[shp[hstart % npm]][11] && cchnv[shp[hstart % npm]][3] && hvar == 2 && !found_de4) {
 		FlagA(0, 375, hstart, hstart, 0, 3);
